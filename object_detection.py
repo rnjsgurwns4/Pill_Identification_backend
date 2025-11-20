edfa4eaeda45ffa123a1dec732a661a7d2af92f7
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-11-20 12:50:14 +0900

    commit: 주석 정리

--- object_detection.py
+++ object_detection.py
@@ -3,14 +3,10 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-# 알약 탐지 함수
 def detect_pills(image_path, model_path='weights/detection_model.pt'):
-    """
-    YOLOv8 모델을 사용하여 이미지에서 알약 객체를 탐지,
-    수동 NMS(비최대 억제)를 적용하여 중복 박스를 확실하게 제거
-    """
+# YOLOv8 모델을 사용하여 이미지에서 알약 객체를 탐지, 수동 NMS(비최대 억제)를 적용하여 중복 박스를 확실하게 제거
+
     try:
-        # 모델 불러오기
         model_path = 'weights/detection_model.pt'
         model = YOLO(model_path)
         
@@ -35,8 +31,6 @@ def detect_pills(image_path, model_path='weights/detection_model.pt'):
             return []
 
         # OpenCV의 NMSBoxes 함수를 사용하여 중복 박스 제거
-        #    score_threshold: 이 신뢰도 이하의 박스는 고려하지 않음
-        #    nms_threshold: 이 iou 값 이상 겹치는 박스는 중복으로 보고 제거
         indices = cv2.dnn.NMSBoxes(raw_boxes, confidences, score_threshold=0.25, nms_threshold=0.4)
         
         final_boxes = []
@@ -59,4 +53,5 @@ def detect_pills(image_path, model_path='weights/detection_model.pt'):
         h, w, _ = dummy_image.shape
         return [
             [int(w*0.1), int(h*0.1), int(w*0.4), int(h*0.4)],
+
         ]
