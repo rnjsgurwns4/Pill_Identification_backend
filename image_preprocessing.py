9448d05fa6d6562a13524c701ac0bb587ba73b51
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-11-25 10:40:36 +0900

    commit: 전처리 수정

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -3,9 +3,9 @@ import numpy as np
 import logging
 
 
-def remove_background(cropped_image):
+def remove_background(pill_image):
 # GrabCut 알고리즘을 사용하여 이미 잘라낸 알약 이미지에서 배경을 제거
-    
+    """
     try:
         h, w = cropped_image.shape[:2]
         if h < 10 or w < 10: return cropped_image, np.zeros((h, w), np.uint8)
@@ -21,6 +21,60 @@ def remove_background(cropped_image):
         logging.error(f"배경 제거 중 오류 발생: {e}", exc_info=True)
         h, w = cropped_image.shape[:2]
         return cropped_image, np.zeros((h, w), np.uint8)
+    """
+    if pill_image is None or pill_image.size == 0:
+        blank_mask = np.zeros((100, 100), dtype="uint8")
+        blank_image = np.zeros((100, 100, 3), dtype="uint8")
+        return blank_image, blank_mask
+    
+    
+    # 마스크: 객체와 배경을 구분하는 정보를 저장할 행렬. 0으로 초기화
+    # bgdModel, fgdModel: 배경과 전경의 색상 분포 모델을 저장할 임시 배열
+    mask = np.zeros(pill_image.shape[:2], np.uint8)
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    
+    # 이미지의 가로나 세로가 너무 작으면 Otsu 이진화를 사용
+    h, w = pill_image.shape[:2]
+    if h < 20 or w < 20:
+        gray = cv2.cvtColor(pill_image, cv2.COLOR_BGR2GRAY) #흑백화
+        _, final_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        result = cv2.bitwise_and(pill_image, pill_image, mask=final_mask)
+        return result, final_mask
+
+    # 알약이 이미지에 꽉 차 있을 것을 대비해, 마진을 1픽셀로 최소화
+    rect = (1, 1, w - 2, h - 2)
+
+    # GrabCut 알고리즘 실행
+    try:
+        cv2.grabCut(pill_image, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    # 실패 시 Otsu 이진화
+    except Exception as e:
+        print(f"    - GrabCut 실패: {e}. Otsu 방식으로 전환합니다.")
+        gray = cv2.cvtColor(pill_image, cv2.COLOR_BGR2GRAY)
+        _, final_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        result = cv2.bitwise_and(pill_image, pill_image, mask=final_mask)
+        return result, final_mask
+
+    # 0: 확실한 배경, 2: 아마도 배경 -> 0 (배경)으로 변경
+    # 1: 확실한 전경, 3: 아마도 전경 -> 1 (전경)으로 변경
+    # np.where 함수를 이용해 최종적으로 배경은 0, 전경(알약)은 1인 바이너리 마스크를 생성
+    final_mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+    
+    
+    # 노이즈 제거 (Morphology 연산)
+    # 마스크에 생길 수 있는 작은 구멍(노이즈)을 메우기
+    # MORPH_ELLIPSE: 타원 모양의 커널(필터)을 생성
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    # MORPH_CLOSE: 팽창(Dilate) 후 침식(Erode)을 수행하여 객체 내부의 작은 구멍을 채우기
+    final_mask = cv2.morphologyEx(final_mask, cv2.MORPH_CLOSE, kernel, iterations=2)
+    
+    
+    result = pill_image * final_mask[:, :, np.newaxis]
+    
+    final_mask_255 = final_mask * 255
+    
+    return result, final_mask_255
 
 
 def preprocess_for_dark_text(image, pill_mask):
@@ -99,3 +153,4 @@ def preprocess_for_tesseract(image, pill_mask):
     logging.warning("이 함수(preprocess_for_tesseract)는 사용되지 않아야 합니다. imprint_analysis.py를 확인하세요.")
 
     return np.full_like(image, 255, dtype=np.uint8)
+
